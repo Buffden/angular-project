e44b1925e29fd7fbe35fcece65f2ead0d53b26ee
Harshwardhan Patil <buffdenslays@gmail.com>
2023-11-25 19:56:43 +0530

    console changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,10 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log('appcomponent: OnInit implemented');
   }
+
   title = 'angular-project';
+
+  onClickEvent(event: any) {
+    console.log(event);
+  }
 }
